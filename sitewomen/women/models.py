cd13937a76f1a92493c83fe5e3170b37209a9472
Павел Рафальский <p.rafalskij@pnevmoteh.local>
2025-01-05 15:16:59 +0300

    #29. Добавление тегов на сайт | Уроки по Django 4

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -19,6 +19,7 @@ class Women(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
     cat = models.ForeignKey("Category", on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField("TagPost", blank=True, related_name='tags')
 
     objects = models.Manager()
     published = PublishedManager()
@@ -46,3 +47,12 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_slug': self.slug})
 
+class TagPost(models.Model):
+    tag = models.CharField(max_length=100, db_index=True)
+    slug = models.CharField(max_length=255, db_index=True)
+
+    def __str__(self):
+        return self.tag
+
+    def get_absolute_url(self):
+        return reverse('tag', kwargs={'tag_slug': self.slug})
